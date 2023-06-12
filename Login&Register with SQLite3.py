a2f1850606e8016cac9b6ca5ae8c02a3fac2923b
MSC_Steve <111713110+SteveProkovas@users.noreply.github.com>
2023-06-12 23:28:03 +0300

    Update Login&Register with SQLite3.py

--- Login&Register with SQLite3.py
+++ Login&Register with SQLite3.py
@@ -1,31 +1,33 @@
 import sqlite3
 
+# Establish a connection to the SQLite database
 conn = sqlite3.connect('dbusers.db')
 c = conn.cursor()
 
+# Create a table named 'users' if it doesn't exist
 c.execute('''CREATE TABLE IF NOT EXISTS users
              (id INTEGER PRIMARY KEY AUTOINCREMENT,
               email TEXT UNIQUE,
               password TEXT)''')
 
-
+# Function to check if a user with the given email already exists
 def check_user_exists(email):
     c.execute("SELECT * FROM users WHERE email=?", (email,))
     result = c.fetchone()
     return result is not None
 
-
+# Function to create a new user in the database
 def create_user(email, password):
     c.execute("INSERT INTO users (email, password) VALUES (?, ?)", (email, password))
     conn.commit()
 
-
+# Function to authenticate a user with the given email and password
 def authenticate_user(email, password):
     c.execute("SELECT * FROM users WHERE email=? AND password=?", (email, password))
     result = c.fetchone()
     return result is not None
 
-
+# Prompt the user for login or signup
 while True:
     choice = input("Enter 'login' to login or 'signup' to sign up: ")
 
